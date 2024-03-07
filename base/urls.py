85d73e0fb38a75eda13d86cf03ffa29662e88fbf
iamoros <ivan13ap@gmail.com>
2024-03-07 01:03:37 +0100

    studies identation

--- base/urls.py
+++ base/urls.py
@@ -7,8 +7,8 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
 	path('skills/', TechnicalSkillCategoryList.as_view()),
 	path('categories/create/', TechnicalSkillCategoryCreate.as_view(), name='category-create'),
-    path('categories/<int:pk>/', TechnicalSkillCategoryUpdate.as_view(), name='technical-skill-category-update'),
-    path('skills/create/', TechnicalSkillCreate.as_view(), name='skill-create'),
+	path('categories/<int:pk>/', TechnicalSkillCategoryUpdate.as_view(), name='technical-skill-category-update'),
+	path('skills/create/', TechnicalSkillCreate.as_view(), name='skill-create'),
 	path('skills/<int:pk>/', TechnicalSkillUpdate.as_view(), name='technical-skill-update'),
 
 	path('works/', WorkExperienceList.as_view()),
@@ -16,6 +16,6 @@ urlpatterns = [
 	path('studies/', StudiesList.as_view()),
 
 	path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+	path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 	path('api/validate_token/', ValidateTokenView.as_view(), name='validate_token'),
 ]
