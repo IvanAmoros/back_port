6d34febd13b2beab9bfa9c6405fecbaf3c553cd4
iamoros <ivan13ap@gmail.com>
2024-03-09 12:05:21 +0100

    Comment model and view

--- base/admin.py
+++ base/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from base.models import TechnicalSkillCategory, TechnicalSkill, WorkExperience, Studies
+from base.models import Comment, TechnicalSkillCategory, TechnicalSkill, WorkExperience, Studies
 
+admin.site.register(Comment)
 admin.site.register(TechnicalSkillCategory)
 admin.site.register(TechnicalSkill)
 admin.site.register(WorkExperience)
