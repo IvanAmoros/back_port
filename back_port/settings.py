71038a4672dcef83b251be27d13b252292256d4a
iamoros <ivan13ap@gmail.com>
2024-06-10 17:50:32 +0200

    doc first version

--- back_port/settings.py
+++ back_port/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- back_port/urls.py
+++ back_port/urls.py
@@ -18,11 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .swagger import schema_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('base/', include('base.urls')),
     path('film-festival/', include('film_festival.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
